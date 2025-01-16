820f8b3e31fe09b6fe7476c39c58678beddb0772
João Lucas <ylucasslalves@gmail.com>
2025-01-16 19:01:16 -0300

    settings config files dirs

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (BASE_DIR / 'static_files')
+STATICFILES_DIRS = (BASE_DIR / 'static_files',)
 STATIC_ROOT = BASE_DIR / 'static'
 
 
