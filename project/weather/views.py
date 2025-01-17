945edcec5ab78ee07a2a7d8d4b2786102306990b
João Lucas <ylucasslalves@gmail.com>
2025-01-17 10:42:40 -0300

    view index e view get_city usando api

--- project/weather/views.py
+++ project/weather/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from weather.forms import CityNameForm
+import json
+import requests
 
 # Create your views here.
+def index(request):
+
+  form = CityNameForm()
+
+  context = {
+    'form': form,
+  }
+
+  return render(request, 'index.html', context)
+
+
+def get_city(request):
+  if request.method == "POST":
+    city_name = request.POST.get('city_name')
+    API_KEY = "858ac266a21efbe6a36fa07337cb5c7c"
+    URL = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&lang=pt_br"
+    response = requests.get(URL)
+    if response.status_code == 200:
+      data = response.json()
+      print(data["name"])
+    else:
+      print("Cidade não encontrada")
+
+  return redirect('index')
