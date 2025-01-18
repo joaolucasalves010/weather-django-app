b69b71128376c348e1500eb52a9689a917146ea3
João Lucas <ylucasslalves@gmail.com>
2025-01-17 22:16:51 -0300

    pegando info da api

--- project/weather/views.py
+++ project/weather/views.py
@@ -10,6 +10,7 @@ def index(request):
 
   context = {
     'form': form,
+    'class': 'hide',
   }
 
   return render(request, 'index.html', context)
@@ -19,12 +20,20 @@ def get_city(request):
   if request.method == "POST":
     city_name = request.POST.get('city_name')
     API_KEY = "858ac266a21efbe6a36fa07337cb5c7c"
-    URL = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&lang=pt_br"
+    URL = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&lang=pt_br&units=metric"
     response = requests.get(URL)
     if response.status_code == 200:
       data = response.json()
-      print(data["name"])
+      print(json.dumps(data, indent=2))
+      context = {
+        "class": "show",
+        "form": CityNameForm(request.POST),
+        "city_name": data["name"],
+        "country_flag": data["sys"]["country"],
+        "icon": data["weather"][0]["icon"],
+        "temperature": data["main"]["temp"],
+        "description": data["weather"][0]["description"],
+      }
+      return render(request, 'index.html', context)
     else:
-      print("Cidade não encontrada")
-
-  return redirect('index')
+      return redirect('index')
